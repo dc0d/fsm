73f77171667b419073f0f13ae49da553dafba823
dc0d <kaveh.shahbazian@gmail.com>
2017-10-02 11:14:49 +0330

    initial commit; moved here

---
+++ fsm_test.go
@@ -0,0 +1,59 @@
+package fsm
+
+import (
+	"testing"
+
+	"github.com/dc0d/club/errors"
+	"github.com/stretchr/testify/assert"
+)
+
+var (
+	errNegative = errors.Errorf("NEGATIVE")
+)
+
+type sample struct {
+	state int
+}
+
+func (s *sample) Start() State {
+	return StateFunc(s.dispatcher)
+}
+
+func (s *sample) odd() (State, error) {
+	s.state++
+	return StateFunc(s.dispatcher), nil
+}
+
+func (s *sample) even() (State, error) {
+	s.state++
+	return StateFunc(s.dispatcher), nil
+}
+
+func (s *sample) dispatcher() (State, error) {
+	if s.state > 8 {
+		return nil, nil
+	}
+	if s.state < 0 {
+		return nil, errNegative
+	}
+	if s.state%2 == 0 {
+		return StateFunc(s.even), nil
+	}
+	return StateFunc(s.odd), nil
+}
+
+func Test01(t *testing.T) {
+	assert := assert.New(t)
+
+	fsm := &sample{}
+	err := Activate(fsm.Start())
+
+	assert.Equal(9, fsm.state)
+	assert.Equal(nil, err)
+
+	fsm.state = -10
+	err = Activate(fsm.Start())
+
+	assert.Equal(-10, fsm.state)
+	assert.Equal(errNegative, err)
+}
